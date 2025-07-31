f0d2b469f860bf147960280cc2b7a2a8b395149e
khaled <khaled.gaguiga@holbertonschool.com>
2025-07-31 10:30:45 +0100

    new task 7 product detail

--- data_analysis/web_scraping/7-prouct_detail.py
+++ data_analysis/web_scraping/7-prouct_detail.py
@@ -14,7 +14,8 @@ def scrape_product_detail(url, delay=2.0):
     driver.get(url)
     time.sleep(delay)  # ensure content loads
 
-    title_el = driver.find_element(By.CSS_SELECTOR, ".caption h4:nth-of-type(2)")
+    title_el = driver.find_element(By.CSS_SELECTOR,
+                                   ".caption h4:nth-of-type(2)")
     price_el = driver.find_element(By.CSS_SELECTOR, ".price")
     desc_el = driver.find_element(By.CSS_SELECTOR, ".description")
     stars = driver.find_elements(By.CSS_SELECTOR, ".ratings .glyphicon-star")

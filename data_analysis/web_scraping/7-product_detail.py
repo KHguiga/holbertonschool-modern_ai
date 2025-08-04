347e69f4395611088b22ee68e5393c6ede9ad0d2
khaled <khaled.gaguiga@holbertonschool.com>
2025-08-04 23:35:34 +0100

    trying with only webdriver import task 7

--- data_analysis/web_scraping/7-product_detail.py
+++ data_analysis/web_scraping/7-product_detail.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python3
 import time
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By
 
 
 def scrape_product_detail(url, delay=2.0):
-    opts = Options()
+    opts = webdriver.chrome.options.Options()
     opts.add_argument("--headless")
     opts.add_argument("--no-sandbox")
     opts.add_argument("--disable-gpu")
@@ -14,11 +12,14 @@ def scrape_product_detail(url, delay=2.0):
     driver.get(url)
     time.sleep(delay)  # ensure content loads
 
-    title_el = driver.find_element(By.CSS_SELECTOR,
+    title_el = driver.find_element(webdriver.common.by.By.CSS_SELECTOR,
                                    ".caption h4:nth-of-type(2)")
-    price_el = driver.find_element(By.CSS_SELECTOR, ".price")
-    desc_el = driver.find_element(By.CSS_SELECTOR, ".description")
-    stars = driver.find_elements(By.CSS_SELECTOR, ".ratings .glyphicon-star")
+    price_el = driver.find_element(webdriver.common.by.By.CSS_SELECTOR,
+                                   ".price")
+    desc_el = driver.find_element(webdriver.common.by.By.CSS_SELECTOR,
+                                  ".description")
+    stars = driver.find_elements(webdriver.common.by.By.CSS_SELECTOR,
+                                 ".ratings .glyphicon-star")
 
     result = {
         "title":       title_el.text.strip(),

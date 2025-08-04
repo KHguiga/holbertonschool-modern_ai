4544811e81a2e30ddc34fc3e55f58eea090d4b2c
khaled <khaled.gaguiga@holbertonschool.com>
2025-08-04 23:17:36 +0100

    trying with only webdriver import

--- data_analysis/web_scraping/6-products_list.py
+++ data_analysis/web_scraping/6-products_list.py
@@ -1,25 +1,24 @@
 #!/usr/bin/env python3
 import time
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 
 
 def scrape_products(url, delay=2.0):
-    opts = Options()
+    opts = webdriver.chrome.options.Options()
     opts.add_argument("--headless")
     opts.add_argument('--no-sandbox')
     driver = webdriver.Chrome(options=opts)
     driver.get(url)
     time.sleep(delay)  # let static content load
 
-    cards = driver.find_elements(By.CSS_SELECTOR, ".thumbnail")
+    cards = driver.find_elements(webdriver.common.by.By.CSS_SELECTOR, ".thumbnail")
     results = []
     for card in cards:
-        title_el = card.find_element(By.CSS_SELECTOR, "a.title")
-        price_el = card.find_element(By.CSS_SELECTOR, "h4.price")
-        desc_el = card.find_element(By.CSS_SELECTOR, "p.description")
-        stars = card.find_elements(By.CSS_SELECTOR, ".ratings .glyphicon-star")
+        title_el = card.find_element(webdriver.common.by.By.CSS_SELECTOR, "a.title")
+        price_el = card.find_element(webdriver.common.by.By.CSS_SELECTOR, "h4.price")
+        desc_el = card.find_element(webdriver.common.by.By.CSS_SELECTOR, "p.description")
+        stars = card.find_elements(webdriver.common.by.By.CSS_SELECTOR, ".ratings .glyphicon-star")
 
         results.append({
             "title":       title_el.get_attribute("title").strip(),

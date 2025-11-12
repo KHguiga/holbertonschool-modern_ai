8ce3bf310751fce60e45c4b64791c9dedf22a008
khaled <khaled.gaguiga@holbertonschool.com>
2025-11-12 13:46:14 +0100

    task 2 tokenization with 3 methods

---
+++ nlp/introduction_to_nlp/2-tokenize.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""
+    Task 2
+"""
+import nltk
+
+
+def tokenize_text(text, method="tweet"):
+    """
+    Tokenize SMS text.
+
+    Options:
+      - 'tweet' : TweetTokenizer (handles emojis, repeated punctuation, slang)
+      - 'word'  : nltk.word_tokenize (standard NLTK tokenizer)
+      - 'split' : simple whitespace split
+
+    Returns:
+        List of tokens
+    """
+    if not isinstance(text, str):
+        return []
+
+    if method == "tweet":
+        tokenizer = nltk.tokenize.TweetTokenizer(strip_handles=True,
+                                                 reduce_len=True)
+        return tokenizer.tokenize(text)
+    elif method == "word":
+        return nltk.tokenize.word_tokenize(text)
+    elif method == "split":
+        return text.split()
+    else:
+        raise ValueError("Invalid tokenizer method")

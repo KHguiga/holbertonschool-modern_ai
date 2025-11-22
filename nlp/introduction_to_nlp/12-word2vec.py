f8f83e3b9d16e35c6c29f1662cf2b064bb031246
khaled <khaled.gaguiga@holbertonschool.com>
2025-11-22 16:13:19 +0100

    task 12 word2vec

--- nlp/introduction_to_nlp/12-word2vec.py
+++ nlp/introduction_to_nlp/12-word2vec.py
@@ -7,7 +7,7 @@ import gensim
 
 
 def train_word2vec(corpus_tokens, vector_size=100, window=5,
-                   min_count=1, sg=1, epochs=50, save_path=None):
+                   min_count=1, sg=1, epochs=50):
     """
     Train a Word2Vec model from a tokenized corpus.
 
@@ -18,7 +18,6 @@ def train_word2vec(corpus_tokens, vector_size=100, window=5,
         min_count (int): Minimum frequency threshold
         sg (int): Skip-gram=1, CBOW=0
         epochs (int): Number of iterations
-        save_path (str or None): If provided, save model to this path
 
     Returns:
         gensim.models.Word2Vec
@@ -32,8 +31,5 @@ def train_word2vec(corpus_tokens, vector_size=100, window=5,
         sg=sg,
         epochs=epochs
     )
-
-    if save_path:
-        model.save(save_path)
 
     return model

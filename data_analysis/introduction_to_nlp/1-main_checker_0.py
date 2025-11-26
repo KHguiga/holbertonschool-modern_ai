4294dfb80bcd8cda18ba0922a585ec65d0d9178f
khaled <khaled.gaguiga@holbertonschool.com>
2025-11-26 12:48:45 +0100

    Remove local files from introduction_to_nlp folder (__pycache__ and *-main) and update gitignore

--- data_analysis/introduction_to_nlp/1-main_checker_0.py
+++
@@ -1,9 +0,0 @@
-clean_text = __import__('1-clean_text').clean_text
-
-"""
-Tests lowercasing, whitespace stripping, and HTML entity decoding.
-"""
-text = "   HeLLo &amp; WORLD!   "
-cleaned = clean_text(text)
-
-print(repr(cleaned))
--- data_analysis/introduction_to_nlp/2-main_checker_0.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-tokenize_text = __import__('2-tokenize').tokenize_text
-
-"""
-Tests basic tokenization on simple text.
-"""
-text = "hello world this is a test"
-tokens = tokenize_text(text, method="split")
-print(repr(tokens), end="")

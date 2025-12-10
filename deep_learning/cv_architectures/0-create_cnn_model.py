aaf8aa6236e036ef7d000fd0a9e139e1733b60e8
khaled <khaled.gaguiga@holbertonschool.com>
2025-12-10 12:29:05 +0100

    Computer Vision Architectures __ Task 0 Creating CNN architecture

---
+++ deep_learning/cv_architectures/0-create_cnn_model.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""
+    Task 0
+"""
+from tensorflow import keras
+
+
+def create_cnn_model(input_shape, filters, kernel_sizes,
+                     activations, pooling_type='max'):
+    model = keras.Sequential()
+    model.add(keras.layers.Input(shape=input_shape))
+
+    for f, s, a in zip(filters, kernel_sizes, activations):
+        model.add(keras.layers.Conv2D(f, s, activation=a))
+        if pooling_type == 'max':
+            model.add(keras.layers.MaxPooling2D((2, 2)))
+        elif pooling_type == 'avg':
+            model.add(keras.layers.AveragePooling2D((2, 2)))
+        else:
+            raise ValueError(f"Unsupported pooling type: {pooling_type}")
+
+    model.add(keras.layers.Flatten())
+    model.add(keras.layers.Dense(10, activation='softmax'))
+
+    return model

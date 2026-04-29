b7ff0ea22369e7bfbb0b1dba6b667778b5514e9f
khaled <khaled.gaguiga@holbertonschool.com>
2026-04-29 09:19:59 +0100

    Tree_based - Task 2

---
+++ machine_learning/tree_models/2-draw.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+Task 2: View the Decision Rules of a Trained Tree
+"""
+from sklearn import tree
+
+def draw(clf, feature_names, class_names):
+    r =tree.export_text(clf, feature_names=feature_names, class_names=class_names)
+    print(r)

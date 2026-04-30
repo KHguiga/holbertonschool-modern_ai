5cae1fe22b3d75f4dfe7abc899f06e358a2ac7a1
khaled <khaled.gaguiga@holbertonschool.com>
2026-04-30 13:23:18 +0100

    Tree_Based - Task 5

--- machine_learning/tree_models/5-pre_pruning.py
+++ machine_learning/tree_models/5-pre_pruning.py
@@ -6,10 +6,10 @@ from sklearn import model_selection
 
 def prepruning(X, y, clf):
     params_grid = {
-        'criteria': ('gini', 'entropy'),
-        'max_depth': range(1, 5),
-        'min_samples_leaf': range(1, 5),
-        'min_samples_split': range(1, 5)
+        'criterion': ('gini', 'entropy'),
+        'max_depth':  range(2, 6),
+        'min_samples_leaf':  range(2, 6),
+        'min_samples_split':  range(2, 6)
     }
     g_cv = model_selection.GridSearchCV(clf, param_grid=params_grid)
     g_cv.fit(X=X, y=y)

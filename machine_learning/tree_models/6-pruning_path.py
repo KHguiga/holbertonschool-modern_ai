295fa1c261c98c5f186a998a2db87ccca2052ec6
khaled <khaled.gaguiga@holbertonschool.com>
2026-04-30 15:24:22 +0100

    Tree_Based - Task 6

--- machine_learning/tree_models/6-pruning_path.py
+++ machine_learning/tree_models/6-pruning_path.py
@@ -7,4 +7,5 @@ Task 6: Post-Pruning cost complexity
 def get_pruning_path(clf, X, y):
     """
     """
-    return clf.cost_complexity_pruning_path(X, y)
+    path = clf.cost_complexity_pruning_path(X, y)
+    return path.ccp_alphas, path.impurities
